8559ac26e0e83396a4dedd514e1f56059580d884
Dario Kupec <dariokupec@gmail.com>
2024-07-17 15:20:43 -0400

    creacion de la clase configuracion

---
+++ automata-celular/src/Configuracion.java
@@ -0,0 +1,22 @@
+public final class Configuracion {
+    //parametros del tablero
+    public static final int ANCHO_TABLERO = 10;
+    public static final int ALTO_TABLERO = 10;
+
+    //parametros de los seres vivos
+
+    //parametros de las plantas
+    public static final int ENERGIA_INICIAL_PLANTA = 5;
+    public static final int ENERGIA_MAXIMA_PLANTA = 100;
+
+    //parametros de los animales
+    public static final int ENERGIA_INICIAL_ANIMAL = 10;
+    public static final int ENERGIA_MAXIMA_ANIMAL = 100;
+    public static final int EDAD_MAXIMA_ANIMAL = 20;
+
+    //parametros del archivo de salida
+    public static final String ARCHIVO_DE_SALIDA = "estadisticas.csv";
+
+    private Configuracion() {}
+
+}
