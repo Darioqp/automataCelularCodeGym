8146820c6f0785a222ede9d0a87ba226f2b66943
Dario Kupec <dariokupec@gmail.com>
2024-07-18 17:49:44 -0400

    modificacion de la clase Celda

--- automata-celular/src/Celda.java
+++ automata-celular/src/Celda.java
@@ -2,11 +2,64 @@ public class Celda {
 
     private int indiceFila;
     private int indiceColumna;
-    private String estado;
+    private EstadoCelda estado;
+    private SerVivo serVivo;
 
 
-    public Celda(int indiceFila, int indiceColumna) {
+    public Celda(int indiceFila, int indiceColumna, EstadoCelda estadoInicial) {
         this.indiceFila = indiceFila;
         this.indiceColumna = indiceColumna;
+        this.estado = estadoInicial;
+        this.serVivo = null;
+    }
+
+    //metodos get y set para los atributos
+    public int getIndiceFila() {
+        return indiceFila;
+    }
+
+    public int getIndiceColumna() {
+        return indiceColumna;
+    }
+
+    public EstadoCelda getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoCelda nuevoEstado) {
+        this.estado = nuevoEstado;
+    }
+
+    public SerVivo getSerVivo() {
+        return serVivo;
+    }
+
+    public void setSerVivo(SerVivo serVivo) {
+        this.serVivo = serVivo;
+    }
+
+    // Métodos auxiliares para verificar el estado de la celda
+    public boolean estaVacia() {
+        return estado == EstadoCelda.VACIO;
+    }
+
+    public boolean tienePlanta() {
+        return estado == EstadoCelda.PLANTA;
+    }
+
+    public boolean tieneAnimal() {
+        return estado == EstadoCelda.ANIMAL;
+    }
+
+    public boolean estaEnReproduccion() {
+        return estado == EstadoCelda.REPRODUCCION;
+    }
+
+    public boolean estaEnAlimentacion() {
+        return estado == EstadoCelda.ALIMENTACION;
+    }
+
+    public boolean esIndefinido() {
+        return estado == EstadoCelda.INDEFINIDO;
     }
 }
